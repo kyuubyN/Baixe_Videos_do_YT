ee02fd3e2122d08c18bbbf853cfa45424aca88ff
Kauê Oliveira Costa <102341451+kyuubyN@users.noreply.github.com>
2022-05-11 23:45:16 -0300

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -5,3 +5,4 @@ youtube_url = 'https://www.youtube.com/watch?v=BiERAq5oe3c' #Coloque a URL do vi
 ytvid = pytube.YouTube(youtube_url)
 video = ytvid.streams.get_lowest_resolution() #Resolução do video
 video.download()
+#Para maior facilidade utilize o pycharm
